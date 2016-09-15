e0340ca77ae276c1c1dfaf850f770e84aedbd7b2
de_er <416724285@qq.com>
2016-09-15 14:01:13 +0800

    add testmodel to project just test the model

--- delivAdd/models.py
+++ delivAdd/models.py
@@ -1,5 +1,9 @@
+#-*-coding:utf-8-*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+	RecipientsName=models.CharField(('china provinces', 'name'),max_length=12)
