710838d62822a056e12ca3cf504fce0144ffec88
de_er <416724285@qq.com>
2016-09-16 23:41:22 +0800

    the real first version of delivery address

--- delivAdd/models.py
+++ delivAdd/models.py
@@ -5,5 +5,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
 class Address(models.Model):
-	RecipientsName=models.CharField(('china provinces', 'name'),max_length=12)
+    recipent_name=models.CharField("姓名", max_length = 20，blank = False)
+    cellphone=models.CharField("电话号码", max_length = 11，blank = False)
+    province_name = models.CharField(verbose_name="省",max_length = 7, blank = False)
+    city = models.ForeignKey(province_name,verbose_name='城市',blank = False)
+    street = models.ForeignKey(city,verbose_name='街道',blank = False)
+    postal_code = models.CharField("邮政编号", max_length = 10)
+    address_tag = models.CharField("地质标签", max_length = 10)
--- delivAdd/views.py
+++ delivAdd/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from delivAdd.models import Address
+from delivAdd.serializer import AddressSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
